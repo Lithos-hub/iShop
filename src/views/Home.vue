4d262955b30960b9efa05b8a97bcf484f8a840ed
unknown <lithos.contact@gmail.com>
2022-05-01 17:02:54 +0200

    Added nice animations on Home tech icons

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,17 +3,17 @@
     <h2 class="text__gradient--primary">Fake Store made with</h2>
     <div class="techIcon__row">
       <div class="techIcon__wrapper">
-        <img src="../assets/img/vue.png" width="80" height="80" />
-        <p class="text--vue">Vue</p>
+        <img class="vue__image" src="../assets/img/vue.png" width="80" height="80" />
+        <p class="text--vue">Vue 3</p>
       </div>
       <h1 class="text__gradient--primary">+</h1>
       <div class="techIcon__wrapper">
-        <img src="../assets/img/vite.svg" width="80" height="80" />
+        <img class="vite__image" src="../assets/img/vite.svg" width="80" height="80" />
         <p class="text--vite">Vite</p>
       </div>
       <h1 class="text__gradient--primary">+</h1>
       <div class="techIcon__wrapper">
-        <img src="../assets/img/pinia.svg" width="80" height="80" />
+        <img class="pinia__image" src="../assets/img/pinia.svg" width="80" height="80" />
         <p class="text--pinia">Pinia</p>
       </div>
     </div>
@@ -27,7 +27,7 @@
 import { ref } from "vue";
 
 const mayor = ref(0);
-const minor = ref(5);
+const minor = ref(6);
 const patch = ref(0);
 const version = `V${mayor.value}.${minor.value}.${patch.value}`;
 
@@ -68,5 +68,51 @@ h1 {
   background: #202020;
   border-radius: 5px;
   padding: 10px;
+}
+
+.vue__image {
+  animation: hueRotate cubic-bezier(0.445, 0.05, 0.55, 0.95) 1s infinite alternate-reverse;
+}
+.vite__image {
+  border-radius: 20px;
+  padding: 10px;
+  animation: pulse cubic-bezier(0.075, 0.82, 0.165, 1) 2s infinite alternate-reverse;
+}
+
+.pinia__image {
+  animation: motion cubic-bezier(0.785, 0.135, 0.15, 0.86) 1s infinite alternate-reverse;
+}
+
+@keyframes motion {
+  0% {
+    transform: rotate(0deg);
+    margin-left: 0;
+  }
+  50% {
+    transform: rotate(10deg);
+    margin-left: 15px;
+  }
+  100% {
+    transform: rotate(-10deg);
+    margin-left: -10px;
+  }
+}
+
+@keyframes pulse {
+  0% {
+    box-shadow: inset 0px 0px 25px magenta;
+  }
+  100% {
+    box-shadow: inset 0px 2px 10px rgb(118, 68, 165);
+  }
+} 
+
+@keyframes hueRotate {
+  0% {
+    filter: hue-rotate(0deg) brightness(1);
+  }
+  100% {
+    filter: hue-rotate(-150deg) brightness(1.5);
+  }
 }
 </style>
